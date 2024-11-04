f0e3d2873d555b7e9b5aa65035001071f81a058b
bogi <yashaswiboga@gmail.com>
2024-11-04 14:45:00 +0530

    byy

---
+++ Demo.java
@@ -0,0 +1,8 @@
+public class Demo {
+    public static void main(String[] args)
+    {
+        System.out.println("Github");
+        System.out.println("Github working");
+        System.out.println("Github working presentation");
+    }
+}
---
+++ ppt.java
@@ -0,0 +1,8 @@
+public class ppt{
+    public static void main(String[] args)
+    {
+        System.out.println("Github working presentation");
+        System.out.println("Github working");
+        System.out.println("Github");
+    }
+}
