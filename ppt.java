e2ad796ea204154cd14f789b7f9ec241c37eb04d
yashaswi-03 <yashaswiboga@gmail.com>
2024-11-05 12:42:49 +0530

    Update ppt.java

--- ppt.java
+++ ppt.java
@@ -1,7 +1,7 @@
 public class ppt{
     public static void main(String[] args)
     {
-        System.out.println("Github working");
+        // System.out.println("Github working");
         System.out.println("Github");
     }
 }
